a253242683d3c58e96687b7b7d84b1d95e6cd6a8
ridwanndwip <ridwanndwip@gmail.com>
2023-12-29 22:53:37 +0700

    update variable dart

--- bin/variabel.dart
+++ bin/variabel.dart
@@ -1,37 +1,39 @@
 void main() {
-  // Deklarasi Variable
+   // Deklarasi Variable
   String name;
   name = 'Ridwan Dwi';
 
   // Deklarasi Langsung
-  String Name2 = 'Ridwan';
+  String Name2 = 'Ridwan Permana';
 
-  // Kata Kunci Var
+  // Kata Kunci Var = Nilai variabel dapat diubah/deklarasikan ulang bebas.
   var years = 1996;
   var years2 = 1;
 
-  // Final = agar sebuah variabel tidak dapat di deklarasikan ulang, namun dapat merubah nilai dari variabel tersebut
-  final numberone = 123;
-  // Deklarasikan ulang Variabel
-  var numbertwo = 456;
+  // Kata Kunci Final = NILAI tidak dapat di DEKLARASIKAN ULANG, namun masih dapat MERUBAH
+  // NILAI. bisa menggunakan contoh array. 
+  // Kata Kunci Const = NILAI VARIABEL tidak bisa diubah/deklarasikan ulang & NAMA VARIABEL
+  // tidak dapat diubah/di deklarasikan ulang, contoh.
 
-  //const = variable tidak diubah & nilai tidak diubah, contoh.
   final array1 = [1, 2, 3];
   const array2 = [2, 3, 4];
 
   print(array1[0]);
-  array1[0] = 3;
-  print(array1[0]);
+  // Merubah nilai variabel array1  
+  array1[0] = 3; 
+  print(array1);
 
   // array2[0] = 5;
-  print(array2[0]);
+  print(array2);
 
-  // print(name);
-  // print('Ridwan Dwi Permana');
-  // print(Name2);
-  // print(years + years2);
-  // print(numberone);
-  // print(numbertwo);
-  // numbertwo = 654;
-  // print(numbertwo);
+  // Kata kunci late = 
+  // contoh kasus late :
+  late var value = getValue();
+  print('Variable sudah dibuat');
+  print(value);
+}
+
+String getValue() {
+  print('getValue() dipanggil');
+  return 'Eko  Kurniawan  Khannedy';
 }
